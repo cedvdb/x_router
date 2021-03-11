78864ad1c91a333d9bba8cec90200fc836c3295e
cedvdb <cedvandenbosch@gmail.com>
2021-03-11 14:20:17 +0100

    update example with route resolvers

--- example/lib/router/router.dart
+++ example/lib/router/router.dart
@@ -1,44 +1,14 @@
-import 'package:example/pages/loading_page.dart';
-import 'package:example/pages/sign_in_page.dart';
 import 'package:example/router/routes.dart';
-import 'package:example/services/products_service.dart';
 import 'package:x_router/x_router.dart';
 
-import '../pages/dashboard_page.dart';
-import '../pages/product_details_page.dart';
-import '../pages/products_page.dart';
 import 'auth_resolver.dart';
-import 'product_found_resolver.dart';
 
 final router = XRouter(
   resolvers: [
-    XNotFoundResolver(redirectTo: '/'),
+    XNotFoundResolver(redirectTo: '/', routes: AppRoutes.routes),
     AuthResolver(),
     XRedirectResolver(from: '/', to: AppRoutes.dashboard),
   ],
-  routes: [
-    XRoute(
-      path: AppRoutes.dashboard,
-      builder: (ctx, params) => DashboardPage(),
-    ),
-    XRoute(
-      path: AppRoutes.products,
-      builder: (ctx, params) => ProductsPage(),
-    ),
-    XRoute(
-        path: AppRoutes.productDetail,
-        builder: (ctx, params) => ProductDetailsPage(params['id']),
-        resolvers: [
-          productFoundResolver,
-        ]),
-    XRoute(
-      path: AppRoutes.loading,
-      builder: (ctx, params) => LoadingPage(),
-    ),
-    XRoute(
-      path: AppRoutes.signIn,
-      builder: (ctx, params) => SignInPage(),
-    )
-  ],
+  routes: AppRoutes.routes,
   onRouterStateChanges: (s) => print(s),
 );
--- example/lib/router/routes.dart
+++ example/lib/router/routes.dart
@@ -1,3 +1,11 @@
+import 'package:example/pages/dashboard_page.dart';
+import 'package:example/pages/loading_page.dart';
+import 'package:example/pages/product_details_page.dart';
+import 'package:example/pages/products_page.dart';
+import 'package:example/pages/sign_in_page.dart';
+import 'package:example/router/product_found_resolver.dart';
+import 'package:x_router/x_router.dart';
+
 class AppRoutes {
   static final String home = '/';
   static final String dashboard = '/dashboard';
@@ -5,4 +13,16 @@ class AppRoutes {
   static final String productDetail = '/products/:id';
   static final String loading = '/loading';
   static final String signIn = '/sign-in';
+
+  static final routes = [
+    XRoute(path: dashboard, builder: (ctx, params) => DashboardPage()),
+    XRoute(path: products, builder: (ctx, params) => ProductsPage()),
+    XRoute(
+      path: productDetail,
+      builder: (ctx, params) => ProductDetailsPage(params['id']),
+      resolvers: [productFoundResolver],
+    ),
+    XRoute(path: loading, builder: (ctx, params) => LoadingPage()),
+    XRoute(path: signIn, builder: (ctx, params) => SignInPage()),
+  ];
 }
