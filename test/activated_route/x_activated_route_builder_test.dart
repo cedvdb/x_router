dce0dd73779ec99712d1456e5d587db2684c2ca3
cedvdb <cedvandenbosch@gmail.com>
2021-11-22 01:50:08 +0100

    activated route tests

--- test/activated_route/x_activated_route_builder_test.dart
+++ test/activated_route/x_activated_route_builder_test.dart
@@ -1,72 +1,84 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:x_router/src/activated_route/x_activated_route.dart';
 import 'package:x_router/src/activated_route/x_activated_route_builder.dart';
 import 'package:x_router/x_router.dart';
 
 void main() {
   group('Activated Route Builder', () {
-    final activatedRouteBuilder = XActivatedRouteBuilder(
-      routes: [
-        XRoute(
-          path: '/',
-          builder: (_, __) => Container(),
-          matchChildren: false,
-        ),
-        XRoute(
-          path: '/products',
-          builder: (_, __) => Container(),
-        ),
-        XRoute(
-          path: '/products/:id',
-          builder: (_, __) => Container(),
-        ),
-        XRoute(
-          path: '/preferences',
-          builder: (_, __) => Container(),
-        ),
-      ],
-    );
+    late XActivatedRouteBuilder activatedRouteBuilder;
+    late XActivatedRoute activatedNoMatchChild;
+    late XActivatedRoute activatedMatchChild;
+    late XActivatedRoute activatedWithStack;
+
+    setUp(() {
+      activatedRouteBuilder = XActivatedRouteBuilder(
+        routes: [
+          XRoute(
+            path: '/',
+            builder: (_, __) => Container(),
+            matchChildren: false,
+          ),
+          XRoute(
+            path: '/products',
+            builder: (_, __) => Container(),
+          ),
+          XRoute(
+            path: '/products/:id',
+            builder: (_, __) => Container(),
+          ),
+          XRoute(
+            path: '/preferences',
+            builder: (_, __) => Container(),
+          ),
+        ],
+      );
+      activatedNoMatchChild = activatedRouteBuilder.build('/');
+      activatedMatchChild =
+          activatedRouteBuilder.build('/products?orderBy=creationDate');
+      activatedWithStack =
+          activatedRouteBuilder.build('/products/123/an-unknown-route');
+    });
 
     group('build', () {
-      final activated = activatedRouteBuilder.build('/');
-      final activated2 =
-          activatedRouteBuilder.build('/products?orderBy=creationDate');
-      final activated3 =
-          activatedRouteBuilder.build('/products/123/an-unknown-route');
-
       test('should have the correct path', () {
-        expect(activated.effectivePath, equals('/'));
-        expect(activated2.effectivePath, equals('/products'));
-        expect(
-            activated3.effectivePath, equals('/products/123/an-unknown-route'));
+        expect(activatedNoMatchChild.effectivePath, equals('/'));
+        expect(activatedMatchChild.effectivePath, equals('/products'));
+        expect(activatedWithStack.effectivePath,
+            equals('/products/123/an-unknown-route'));
       });
 
       test(
         'should have the correct effective path',
         () {
-          expect(activated.effectivePath, equals('/'));
-          expect(activated2.effectivePath, equals('/products'));
-          expect(activated3.effectivePath, equals('/products/123'));
+          expect(activatedNoMatchChild.effectivePath, equals('/'));
+          expect(activatedMatchChild.effectivePath, equals('/products'));
+          expect(activatedWithStack.effectivePath, equals('/products/123'));
         },
       );
 
       test('should have the correct parameters', () {
-        expect(activated3.pathParameters['id'], equals('123'));
+        expect(activatedWithStack.pathParameters['id'], equals('123'));
       });
 
       test('should have the correct query parameters', () {
-        expect(activated3.queryParameters['orderBy'], equals('creationDate'));
+        expect(activatedWithStack.queryParameters['orderBy'],
+            equals('creationDate'));
       });
 
       test('should have the correct upstack', () {
-        expect(activated.upstack.length, equals(0));
-        expect(activated2.upstack.length, equals(0));
-        expect(activated3.upstack.length, equals(1));
+        expect(activatedNoMatchChild.upstack.length, equals(0));
+        expect(activatedMatchChild.upstack.length, equals(0));
+        expect(activatedWithStack.upstack.length, equals(1));
       });
     });
 
     group('add', () {
-      test('should add to the upstack', () => null);
+      test('should add to the upstack', () {
+        final added = activatedRouteBuilder.add('/preferences',
+            [activatedWithStack, ...activatedWithStack.upstack]);
+        expect(added.upstack.length, equals(2));
+      });
     });
   });
 }
